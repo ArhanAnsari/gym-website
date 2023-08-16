d2cb2cae97c72e292b2002e22723e877dab23eba
Greg Sithole <gregsithole@hotmail.co.za>
2023-08-16 22:47:22 +0200

    Added enum support to Link component

--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -1,14 +1,16 @@
+import { SelectedPage } from "@/shared/types";
 import AnchorLink from "react-anchor-link-smooth-scroll"
 
 type Props = {
 	page: string;
-	selectedPage: string;
-	setSelectedPage: (value: string) => void;
+	selectedPage: SelectedPage;
+	setSelectedPage: (value: SelectedPage) => void;
 }
 
 const Link = (props: Props) => {
 
-	const lowerCasePage = props.page.toLowerCase().replace(/ /g, "-")
+	const lowerCasePage = props.page.toLowerCase().replace(/ /g, "-") as SelectedPage;
+
 	return (
 		<AnchorLink
 			href={`#${lowerCasePage}`}
