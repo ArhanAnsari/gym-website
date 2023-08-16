488c3a55810ef9c173fa3d93c385508ae42ca8da
Greg Sithole <gregsithole@hotmail.co.za>
2023-08-16 23:45:40 +0200

    Added Home to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import NavBar from "@/scenes/navbar";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
+import Home from "@/scenes/home";
 
 function App() {
 	const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
@@ -23,6 +24,7 @@ function App() {
 	return (
 		<div className='app bg-gray-20'>
 			<NavBar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+			<Home setSelectedPage={setSelectedPage} />
 		</div>
 	)
 }
